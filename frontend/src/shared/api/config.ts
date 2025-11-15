c4c52868efa3ed500b1ee9ebd57201737753a7cc
Sergey Makarov <makserWWWR04@gmail.com>
2025-11-15 06:36:45 +0300

    update frontend

--- frontend/src/shared/api/config.ts
+++ frontend/src/shared/api/config.ts
@@ -1,2 +1,1 @@
-export const API_URL =
-  "https://calculators-swaziland-coated-reported.trycloudflare.com";
+export const API_URL = "https://calculators-swaziland-coated-reported.trycloudflare.com";
